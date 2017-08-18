abb0e0619c99c696790596984f6463f631ad5a7a
chitra <Dhakchitra@gmail.com>
2017-08-18 11:51:17 +0530

    updated json_file_extract file

--- json_file_extract.py
+++ json_file_extract.py
@@ -1,31 +1,32 @@
 import json
-import sys
+#import sys
 
 
 filepath = "/home/chitra/Python-code/input_file.json"
 
 
-x = raw_input("Enter serach name:")
-
-def json_read(x):
-	with open(filepath,'r') as filejson:
-		jsoninputs = json.load(filejson) 
-		for i in jsoninputs:
-        		if (i == x):
-				val = jsoninputs[i]
-            			print val
+def json_read():
+	x = raw_input("Enter serach name:")
+	x=str(x)
+	with open(filepath,'r+') as filejson:
+		jsonData=filejson.read()
+		jsoninputs = json.loads(jsonData)
+		print jsoninputs     
+		for i,j in jsoninputs.items():
+        		if x==i:
+				val = j
+            			print 'Birthdate is :',val
             			break
-			else:
-				y = raw_input("Enter birthdate:")
-				jsoninputs[x]= y
-				with open(filepath,'w') as filejson2:
-					json.dump(jsoninputs,filejson2)
-					jsoninputs = json.dumps(jsoninputs)
-					print jsoninputs
-					break
- 	
 
-json_read(x)
+		else:
+			y = raw_input("Enter birthdate:")
+			jsoninputs[x]= y
+			with open(filepath,'w+') as filejson:   
+				json.dump(jsoninputs,filejson,indent=4)
+				print jsoninputs
+				
+
+json_read()
       		
 
 
